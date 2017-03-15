34035df2abe5f9027f2003e4b14dc10516cfc66f
Juan A. Guzman <juan.guzman07@gmail.com>
2017-03-14 22:49:10 -0400

    Update S3 Access Keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,20 @@
-CarrierWave.configure do |config|
-  #config.fog_provider = "fog/aws"                        # required
-  config.fog_credentials = {
-    :provider => 'AWS',                                     # required
-    :aws_access_key_id => ENV['aws_access_key_id'],         # required
-    :aws_secret_access_key => ENV['aws_secret_access_key']  # required
-  }
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-  config.fog_directory  = ENV['fog_directory']              # required
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'                          # required
+    config.fog_credentials = {
+      provider: 'AWS',                                     # required
+      aws_access_key_id: ENV['aws_access_key_id'],         # required
+      aws_secret_access_key: ENV['aws_secret_access_key']  # required
+    }
+
+    config.fog_directory  = ENV['fog_directory']              # required
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
