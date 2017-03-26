5d1111a0307658f948de23432d942a1928cd1b9b
Juan A. Guzman <juan.guzman07@gmail.com>
2017-03-25 21:29:40 -0400

    Link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325235340) do
+ActiveRecord::Schema.define(version: 20170326011745) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170325235340) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
