f8d244bf0f280b218b78d43046a8007f88a74ee5
Juan A. Guzman <juan.guzman07@gmail.com>
2017-03-27 22:25:31 -0400

    Added field validation and placeholders

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,9 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :phone, format: {with: /\A\(\d{3}\)\s\d{3}-\d{4}\z/, message: "must be in the format: (555) 332-1122"}
+	validates :website, format: {with: /\Ahttps?:\/\/.*\z/, message: "must star with http:// or https://"}
+	validates :address, format: {with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/, message: "must be in the format 350 Fifth Avenue, New York, NY 10010"}
 end
