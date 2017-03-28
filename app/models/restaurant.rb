d6a8b94edff397b90a301223cc27e06b6a7cf78a
Juan A. Guzman <juan.guzman07@gmail.com>
2017-03-27 22:27:42 -0400

    Updated validation message on Restaurant page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,6 @@ class Restaurant < ApplicationRecord
 
 	validates :name, :address, :phone, :website, :image, presence: true
 	validates :phone, format: {with: /\A\(\d{3}\)\s\d{3}-\d{4}\z/, message: "must be in the format: (555) 332-1122"}
-	validates :website, format: {with: /\Ahttps?:\/\/.*\z/, message: "must star with http:// or https://"}
+	validates :website, format: {with: /\Ahttps?:\/\/.*\z/, message: "must start with http:// or https://"}
 	validates :address, format: {with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/, message: "must be in the format 350 Fifth Avenue, New York, NY 10010"}
 end
